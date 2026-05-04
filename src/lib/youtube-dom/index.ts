2bc931556a02856c7cc10188f065dcd3e3bff3e4
dong <me@dong.kim>
2026-05-04 09:41:44 +0900

    feat(youtube-dom): add barrel index re-export

---
+++ src/lib/youtube-dom/index.ts
@@ -0,0 +1,7 @@
+export { YT_REGEX } from './patterns.js';
+export { YT_SELECTORS } from './selectors.js';
+export { warnOnce } from './_warn.js';
+export { extractVideoIdFromHref, getCurrentVideoId } from './video-id.js';
+export { isThumbnailAnchor, findCardHost } from './cards.js';
+export { getCurrentVideoDuration } from './player.js';
+export { findOwnerChannelUc, extractOwnerLabel } from './owner-channels.js';
